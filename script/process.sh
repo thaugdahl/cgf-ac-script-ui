18475de6f4586314aab83c89cbda780debada087
Tor Andre Haugdahl <thaugdahl@hotmail.com>
2022-09-13 22:22:08 +0200

    Bash script stage 1 completed

--- script/process.sh
+++ script/process.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-help_message() {
-    echo "File Processor"
-    echo "Usage: ./process.sh {input_file} {output_file}"
-}
+DEFAULT_IFS=$IFS
+
+# The functionality is implemented in `functions.sh`
+source functions.sh
 
 # Show a help message if not enough arguments
 if [ $# -lt 2 ]; then
@@ -11,38 +11,39 @@ if [ $# -lt 2 ]; then
     exit
 fi
 
+# Read the arguments
 INPUT_FILE=$1
 OUTPUT_FILE=$2
 
-echo "Chosen input file: ${INPUT_FILE}"
-echo "Chosen output file: ${OUTPUT_FILE}"
+# The File Suffix to handle
+FILE_SUFFIX=".GRDECL"
 
-if [ ! -f ${INPUT_FILE} ]; then
-    echo "${INPUT_FILE} not found. Aborting."
-    exit
+
+# Modes:
+# 0 = Single-File Mode
+# 1 = Directory Mode
+MODE=0
+
+echo "Chosen input path: ${INPUT_FILE}"
+echo "Chosen output path: ${OUTPUT_FILE}"
+
+# - Checks existence of input file
+# - Prompts the user for confirmation if about to overwrite output file.
+# - Sets the mode based on whether INPUT_FILE is a directory
+file_checks
+
+# Reads patterns from `patterns.txt` file
+read_patterns
+
+# Default back to default patterns if read_patterns fails
+if [ $result -eq 0 ]; then
+    set_default_patterns
 fi
 
-if [ -f ${OUTPUT_FILE} ]; then
-    echo "${OUTPUT_FILE} exists. Do you want to overwrite it? (y/n)"
-    read ANSWER
-
-    if [ ${ANSWER} != "y" ]; then
-        echo "Aborting..."
-        exit
-    fi
-
-    echo "You have chosen to overwrite ${OUTPUT_FILE}"
+if [ $MODE -eq 1 ]; then
+    replace_entire_directory
+else
+    sed "${SED_PATTERN}" $INPUT_FILE > $OUTPUT_FILE
 fi
 
-PATTERNS_MATCH=("PCW" "PCH" "PCD")
-PATTERNS_REPLACE=("Pth_x" "Pth_y" "Pth_z")
-
-SED_PATTERN=
-
-for x in $(seq 0 $((${#PATTERNS_MATCH[@]} - 1))); do
-    SED_PATTERN="${SED_PATTERN};s/${PATTERNS_MATCH[$x]}/${PATTERNS_REPLACE[$x]}/"
-    echo "Replacing ${PATTERNS_MATCH[$x]} with ${PATTERNS_REPLACE[$x]}"
-done
-
-sed "${SED_PATTERN}" $INPUT_FILE > $OUTPUT_FILE
-
+# -------------- REPLACE PROCEDURE COMPLETED ---------------
